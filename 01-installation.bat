da9eda8c5e36094787e6fcef1c45e0e8798a7e0d
umyuu <124dtiaka@gmail.com>
2025-01-26 09:30:07 +0900

    パッケージ管理をuvに変更しました。

--- 01-installation.bat
+++ 01-installation.bat
@@ -1,10 +1,14 @@
-@echo on
+REM uvのキャッシュパスの変更（SSD最適化）
+REM 通常のキャッシュ場所をSSD上の指定したパス（K:\uv_cache）にリンクすることで、
+REM キャッシュの読み書き速度を最適化します。リンクが必要な場合のみ有効です。
+REM mklink /d %LOCALAPPDATA%\uv\cache K:\uv_cache
 
-cd %~dp0
+REM pip経由でuvをインストール
+pip install uv
 
-python -m venv venv  
-call venv\Scripts\activate
-
-pip install -r requirements.txt  
+REM uvの依存関係を同期
+REM `uv sync`を使って、プロジェクトの依存関係を`pyproject.toml`および`uv lock`ファイルに基づいてインストールまたは更新します。
+REM これにより、環境が同期され、必要なライブラリがインストールされます。
+uv sync --reinstall
 
 TIMEOUT /T 10
